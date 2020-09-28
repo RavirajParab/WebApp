c58b68d956335afea409202906f81aa10bfde337
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-28 13:10:22 +0530

    added regenerator-runtime

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -3,4 +3,5 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 //load the JS
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import regeneratorRuntime from "regenerator-runtime";
 
